7723b25855f6a7cf3c78ead041ce1cfbdfa3081a
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-02-13 10:43:18 +0100

    Cambios relacionados a la sentencia del ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Piloto> pilotos = new ArrayList<Piloto>();
         GenerarDatos.crearPilotos(pilotos);
-        Escritor.escribirPilotos("pilotos.corredores", pilotos);
-        Lector.leerPilotos("pilotos.corredores");
-        Piloto piloto = new Piloto("Sainz","Ferrari",320);
+        Escritor.escribirPilotos("pilotos.dat", pilotos);
+        Lector.leerPilotos("pilotos.dat");
+        System.out.println("Añadir un piloto: ");
+        System.out.println("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Escuderia: ");
+        String escuderia = sc.nextLine();
+        System.out.println("Puntos: ");
+        int puntos = sc.nextInt();
+
+        Piloto piloto = new Piloto(nombre, escuderia, puntos);
         AniadirLista.aniadirPilotos(pilotos, piloto);
         System.out.println("\nBusqueda de un piloto: ");
         BuscarLista.buscarPiloto("Sainz",pilotos);
         System.out.println("\nMostrar pilotos: ");
         MostrarLista.mostrarPilotos(pilotos);
+        sc.nextLine();
     }
 }
