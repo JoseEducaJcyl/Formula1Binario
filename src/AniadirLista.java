89cf81bfc3af3c5f438dc05bfd976429b57d96d3
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:49:10 +0100

    Comentarios agregados

--- src/AniadirLista.java
+++ src/AniadirLista.java
@@ -1,7 +1,9 @@
+// AniadirLista.java
 import java.util.ArrayList;
 
 public class AniadirLista {
+    // Método súper sencillo: añade un piloto a la lista
     public static void aniadirPilotos(ArrayList<Piloto> pilotos, Piloto piloto) {
-        pilotos.add(piloto);
+        pilotos.add(piloto); // Añade el piloto que le pasan al final de la lista
     }
 }
