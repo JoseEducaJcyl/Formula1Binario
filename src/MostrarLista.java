54c137310698198380f4970a7cff0ce9d14a34fb
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:54:15 +0100

    Comentarios agregados

--- src/MostrarLista.java
+++ src/MostrarLista.java
@@ -1,11 +1,16 @@
+// MostrarLista.java
 import java.util.ArrayList;
 
 public class MostrarLista {
+    // Este método muestra todos los pilotos de la lista por pantalla
     public static void mostrarPilotos(ArrayList<Piloto> pilotos) {
+        // Recorro toda la lista con un for-each
         for (Piloto piloto : pilotos) {
+            // Muestro cada dato del piloto
             System.out.println("Piloto: " + piloto.getNombre());
             System.out.println("Escuderia: " + piloto.getEscuderia());
             System.out.println("Puntos: " + piloto.getPuntos());
+            System.out.println(); // Línea en blanco para separar pilotos
         }
     }
 }
