b392ce8b781e949031eaed0a48576f030128da0f
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:51:43 +0100

    Comentarios agregados

--- src/GenerarDatos.java
+++ src/GenerarDatos.java
@@ -1,13 +1,15 @@
+// GenerarDatos.java
 import java.util.ArrayList;
 
 public class GenerarDatos {
+    // Este método crea 4 pilotos de F1 de ejemplo y los mete en la lista
     public static ArrayList<Piloto> crearPilotos(ArrayList<Piloto> pilotos){
+        // Añado pilotos con nombre, escudería y puntos
         pilotos.add(new Piloto("Verstappen","Red Bull",255));
         pilotos.add(new Piloto("Hamilton","Mercedes",190));
         pilotos.add(new Piloto("Alonso","Aston Martin",170));
         pilotos.add(new Piloto("Leclerc","Ferrari",185));
 
-        return pilotos;
-
+        return pilotos; // Devuelvo la lista con los pilotos añadidos
     }
 }
